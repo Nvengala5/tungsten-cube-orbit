0036e4f7f879396a784017f777a71ce246044427
Nivedh <nivedh.vengala@gmail.com>
2024-10-05 15:18:09 -0400

    init

---
+++ components/navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar() {
+
+    return (
+        <div>
+            <div>
+                Tungsten Orbit
+            </div>
+            <div>
+                Alert
+            </div>
+        </div>
+    )
+
+}
